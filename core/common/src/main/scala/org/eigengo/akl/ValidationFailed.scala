8112409c35b8be1cc975c486ad244c8dbdebfdef
Jan Machacek <jan.machacek@gmail.com>
2015-08-25 18:09:51 +0100

    Documentation

--- core/common/src/main/scala/org/eigengo/akl/ValidationFailed.scala
+++ core/common/src/main/scala/org/eigengo/akl/ValidationFailed.scala
@@ -1,7 +1,16 @@
 package org.eigengo.akl
 
+/**
+ * A common supertype for all failed validations
+ */
 trait ValidationFailed
+
+/**
+ * Companion holding typical validation failures
+ */
 object ValidationFailed {
+  /** It's not mean to work yet! */
   case object NotImplementedYet extends ValidationFailed
+  /** The ``data`` is not a valid UUID string */
   case class NotValidUUID(data: String) extends ValidationFailed
 }
